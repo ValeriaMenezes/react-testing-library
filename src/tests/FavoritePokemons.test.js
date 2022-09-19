24e17a054e6187bc5dbe2f49cb965b53ff5d0e23
Valéria <valeriamenezes022@gmail.com>
2022-09-19 18:12:11 -0300

    adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente FavoritePokemons ', () => {
+  it('Teste se é exibida na tela a mensagem esperada', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeDefined();
+  });
+});
