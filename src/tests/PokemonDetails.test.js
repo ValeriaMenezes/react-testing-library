8395f439964d9954f78e091dd1f48a7868e4df7a
Valéria <valeriamenezes022@gmail.com>
2022-09-20 17:37:53 -0300

    corrige erros de lint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
-import { act } from 'react-dom/test-utils';
 
 describe('Testa o componente PokemonDetails', () => {
   it('É exibido na tela um h2 com o texto <name> Details', () => {
